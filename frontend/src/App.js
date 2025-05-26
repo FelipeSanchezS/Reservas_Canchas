8c7423cdae2b0209872d4f2b9a08f0190a22ac3a
FelipeSanchezS <sanchez2001felipe@gmail.com>
2025-05-26 17:04:56 -0500

    Se actualiza configuración y estilo del login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
+// import LoginPage from "./pages/LoginPage";
 import PanelAdmin from "./pages/PanelAdmin";
 import LoginPage1 from "./pages/LoginPage1";
 import "./App.css";
--- frontend/src/pages/LoginPage1.js
+++ frontend/src/pages/LoginPage1.js
@@ -1,8 +1,11 @@
-import react from "react";
+// import react from "react";
 import person from "../Components/Assets/person.png";
 import email from "../Components/Assets/email.png";
 import password from "../Components/Assets/password.png";
 import "./LoginPage1.css";
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage1 = () => {
 
@@ -11,33 +14,55 @@ const LoginPage1 = () => {
     const [mensaje, setMensaje] = useState("");
     const navigate = useNavigate();
 
-    
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+        const res = await axios.post("http://localhost:5000/login", {
+            username,
+            password,
+        });
+        if (res.data.success) {
+            localStorage.setItem("auth", "true");
+            navigate("/panel");
+        } else {
+            setMensaje("Credenciales incorrectas");
+        }
+        } catch (error) {
+        setMensaje("Credenciales incorrectas");
+        }
+    };
 
     return (
-        <div className="container">
-            <div className="header">
-                <div className="text">Inicia Sesión</div>
-                <div className="underline"></div>
-            </div>
-            <div className="inputs">
-                {/* <div className="input">
-                    <img src={email} alt=""/>
-                    <input type="email" placeholder="Email" />
-                </div> */}
-                <div className="input">
-                        <img src={person} alt=""/>
-                        <input type="text" placeholder="Usuario" />
+        <form onSubmit={handleLogin}>
+            <div className="container">
+                <div className="header">
+                    <div className="text">Inicia Sesión</div>
+                    <div className="underline"></div>
                 </div>
-                <div className="input">
-                    <img src={password} alt=""/>
-                    <input type="text" placeholder="Contraseña" />
+                <div className="inputs">
+                    {/* <div className="input">
+                        <img src={email} alt=""/>
+                        <input type="email" placeholder="Email" />
+                    </div> */}
+                    <p className="text-input">Usuario</p>
+                    <div className="input">
+                            <img src={person} alt=""/>
+                            <input type="text" placeholder="Usuario" value={username} onChange={(e) => setUsername(e.target.value)}/>
+                    </div>
+                    <p className="text-input">Contraseña</p>
+                    <div className="input">
+                        <img src={email} alt=""/>
+                        <input type="password" placeholder="Contraseña" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    </div>
+                </div>
+                <div className="submit-container">  
+                    <button className="submit">Ingresar</button>
+                    {mensaje && <p className="error">{mensaje}</p>}
+                    {/* <button className="submit">Registrarse</button> */}
                 </div>
             </div>
-            <div className="submit-container">  
-                <button className="submit">Ingresar</button>
-                {/* <button className="submit">Registrarse</button> */}
-            </div>
-        </div>
+        </form>
+
     );
 }
 
